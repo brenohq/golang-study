98a6e6189b3939e432dd44d5bb7b1d2c5bcfb325
brenohq <brenosc2@hotmail.com>
2023-06-22 18:45:32 -0300

    module definition and usage example

---
+++ app/mypackage/mypackage.go
@@ -0,0 +1,17 @@
+package stuff
+
+import (
+	"strconv"
+)
+
+var Name string = "Breno"
+
+func IntArrToStrArr(intArr []int) []string {
+	var strArr []string
+
+	for _, i := range intArr {
+		strArr = append(strArr, strconv.Itoa(i))
+	}
+
+	return strArr
+}
